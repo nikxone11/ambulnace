7404d8eacbc5937be436623d714f69e0630d3017
Nikhil Mhatre <fcbcluerocks@gmail.com>
2020-12-16 00:43:21 +0530

    edit help

--- src/pages/Tabs/helps.js
+++ src/pages/Tabs/helps.js
@@ -1,24 +1,69 @@
 import React from "react";
-import { IonPage, IonContent } from "@ionic/react";
-import SmallHeader from "../../components/Header/SmallHeader";
-import LargeHeader from "../../components/Header/LargeHeader";
+import {
+  IonPage,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonRouterLink,
+  IonLoading,
+  IonTextarea
+} from "@ionic/react";
+import NavHeader from "../../components/Header/NavHeader";
 
-const helps = () => {
+
+
+const helps = (props) => {
   return (
     <IonPage>
-      <SmallHeader title="Ambulance" />
-      <IonContent fullscreen>
-        <LargeHeader title="Ambulance" />
+      
+      <NavHeader title="Help and Support" />
+      <IonContent>
+        <IonItem lines="full">
+          <IonLabel position="floating">First Name</IonLabel>
+          <IonInput
+            name="first name"
+            type="text"
+            required
+          ></IonInput>
+          </IonItem>
+          <IonItem>
+          <IonLabel position="floating">Last Name</IonLabel>
+          <IonInput
+            name="last name"
+            type="text"
+            required
+          ></IonInput>
+        </IonItem>
+        <IonItem lines="full">
+          <IonLabel position="floating">E-mail</IonLabel>
+          <IonInput
+            name="e-mail"
+            required
+          ></IonInput>
+          </IonItem>
+          <IonItem>
+          <IonLabel position="floating">Message</IonLabel>
+          <IonTextarea 
+            required
+          ></IonTextarea>
+        </IonItem>
+        <IonRow>
+          <IonCol>
+            <IonButton
+              type="submit"
+              color="primary"
+              expand="block"
+            >Send Message
+            </IonButton>
+          </IonCol>
+        </IonRow>
       </IonContent>
-  
-      {/* <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact bootstrapURLKeys={{ key:"AIzaSyCG23Fbx0XQSeS2v1LAOgRPxbVyGWdY3jA" }} defaultCenter={this.props.center} defaultZoom={this.props.zoom}>
-        
-        </GoogleMapReact>
-      </div> */}
-        </IonPage>
+    </IonPage>
   );
 };
-
 
 export default helps;
